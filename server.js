4e10b2c4eb357f9af6b313c8a499301089b7202c
Tyanspops <122063359+Tyanspops@users.noreply.github.com>
2023-06-29 21:08:43 -0700

    Update server.js

--- server.js
+++ server.js
@@ -10,6 +10,19 @@ app.post("/", function(req,res) {
      res.sendFile(__dirname + "/game.html");
 });
 
+app.post("/gameaboutus", function(req,res) {
+     res.sendFile(__dirname + "/aboutus.html");
+});
+app.post("/gameterms", function(req,res) {
+     res.sendFile(__dirname + "/terms.html");
+});
+app.post("/gamefaq", function(req,res) {
+     res.sendFile(__dirname + "/faq.html");
+});
+app.post("/gameblog", function(req,res) {
+     res.sendFile(__dirname + "/blog.html");
+});
+
 app.listen(process.env.PORT || 3000, function() {
     console.log(`Server is running on port ${process.env.PORT}.`);
 });
