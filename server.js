fe906bcbec6a2a1343b0057a611c9abbe2290076
Tyanspops <122063359+Tyanspops@users.noreply.github.com>
2023-06-29 20:12:01 -0700

    Update server.js

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const app = express();
 app.use(express.static("./public"));
 
 app.get("/", function(req,res) {
-    res.sendFile(__dirname + "");
+    res.sendFile(__dirname + "/home.html");
 });
 app.post("/", function(req,res) {
-     res.sendFile(__dirname + "");
+     res.sendFile(__dirname + "/game.html");
 });
 
 app.listen(process.env.PORT || 3000, function() {
