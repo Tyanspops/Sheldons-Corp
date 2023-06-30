a857ea37313c7dccd62b67e391df1b8f447735ed
Tyanspops <122063359+Tyanspops@users.noreply.github.com>
2023-06-29 21:27:12 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express"); 
 const app = express();
 
+
 app.use(express.static("./public"));
+
 
 app.get("/", function(req,res) {
     res.sendFile(__dirname + "/home.html");
@@ -9,6 +11,7 @@ app.get("/", function(req,res) {
 app.post("/", function(req,res) {
      res.sendFile(__dirname + "/game.html");
 });
+
 
 app.post("/gameaboutus", function(req,res) {
      res.sendFile(__dirname + "/aboutus.html");
@@ -22,6 +25,24 @@ app.post("/gamefaq", function(req,res) {
 app.post("/gameblog", function(req,res) {
      res.sendFile(__dirname + "/blog.html");
 });
+
+
+app.post("/homehome", function(req,res) {
+     res.sendFile(__dirname + "/home.html");
+});
+app.post("/homeaboutus", function(req,res) {
+     res.sendFile(__dirname + "/aboutus.html");
+});
+app.post("/hometerms", function(req,res) {
+     res.sendFile(__dirname + "/terms.html");
+});
+app.post("/homefaq", function(req,res) {
+     res.sendFile(__dirname + "/faq.html");
+});
+app.post("/homeblog", function(req,res) {
+     res.sendFile(__dirname + "/blog.html");
+});
+
 
 app.listen(process.env.PORT || 3000, function() {
     console.log(`Server is running on port ${process.env.PORT}.`);
