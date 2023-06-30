59bf2bf1ebbfcc89a8155d0a72b479e65cdbfe6f
Tyanspops <122063359+Tyanspops@users.noreply.github.com>
2023-06-29 21:34:47 -0700

    Update server.js

--- server.js
+++ server.js
@@ -43,6 +43,69 @@ app.post("/homeblog", function(req,res) {
      res.sendFile(__dirname + "/blog.html");
 });
 
+app.post("/aboutushome", function(req,res) {
+     res.sendFile(__dirname + "/home.html");
+});
+app.post("/aboutusaboutus", function(req,res) {
+     res.sendFile(__dirname + "/aboutus.html");
+});
+app.post("/aboutusterms", function(req,res) {
+     res.sendFile(__dirname + "/terms.html");
+});
+app.post("/aboutusfaq", function(req,res) {
+     res.sendFile(__dirname + "/faq.html");
+});
+app.post("/aboutusblog", function(req,res) {
+     res.sendFile(__dirname + "/blog.html");
+});
+
+app.post("/termshome", function(req,res) {
+     res.sendFile(__dirname + "/home.html");
+});
+app.post("/termsaboutus", function(req,res) {
+     res.sendFile(__dirname + "/aboutus.html");
+});
+app.post("/termsterms", function(req,res) {
+     res.sendFile(__dirname + "/terms.html");
+});
+app.post("/termsfaq", function(req,res) {
+     res.sendFile(__dirname + "/faq.html");
+});
+app.post("/termsblog", function(req,res) {
+     res.sendFile(__dirname + "/blog.html");
+});
+
+app.post("/faqhome", function(req,res) {
+     res.sendFile(__dirname + "/home.html");
+});
+app.post("/faqaboutus", function(req,res) {
+     res.sendFile(__dirname + "/aboutus.html");
+});
+app.post("/faqterms", function(req,res) {
+     res.sendFile(__dirname + "/terms.html");
+});
+app.post("/faqfaq", function(req,res) {
+     res.sendFile(__dirname + "/faq.html");
+});
+app.post("/faqblog", function(req,res) {
+     res.sendFile(__dirname + "/blog.html");
+});
+
+app.post("/bloghome", function(req,res) {
+     res.sendFile(__dirname + "/home.html");
+});
+app.post("/blogaboutus", function(req,res) {
+     res.sendFile(__dirname + "/aboutus.html");
+});
+app.post("/blogterms", function(req,res) {
+     res.sendFile(__dirname + "/terms.html");
+});
+app.post("/blogfaq", function(req,res) {
+     res.sendFile(__dirname + "/faq.html");
+});
+app.post("/blogblog", function(req,res) {
+     res.sendFile(__dirname + "/blog.html");
+});
 
 app.listen(process.env.PORT || 3000, function() {
     console.log(`Server is running on port ${process.env.PORT}.`);
